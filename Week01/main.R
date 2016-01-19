666db8c20a8c7bcbded4dbb1fef8328740b95063
nickmckay <nickmckay@users.noreply.github.com>
2016-01-19 13:44:11 -0700

    asd

---
+++ Week01/main.R
@@ -0,0 +1,41 @@
+#Welcome to Earth and Environmental Data Analysis!
+#================
+
+#Week 1, Class 1
+#----------
+
+###Introduction to R and DataJoy, 1
+
+
+#Section 1 - R is a calculator
+
+5+6
+
+#R can also store things as **variables**
+
+rememberThis=5+6
+
+print(rememberThis)
+
+# and then work with those variables as if they were numbers
+
+newAnswer=rememberThis*3
+
+print(newAnswer)
+
+#So what are those hashtags for?
+#comment, comment comment!
+
+#R has a lot of other use, than just a calculator.
+
+#Let's load in some data
+
+
+
+
+
+
+
+
+
+
--- main.R
+++
@@ -1,3 +0,0 @@
-x = seq(0, 2*pi, by=pi/100)
-
-plot(x, sin(x), type="l")
